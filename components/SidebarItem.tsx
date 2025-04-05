bffc784e706d8a3aec488f5fce5bcc4e0f476854
Ben <mail@useit-mw.com>
2025-04-05 17:27:51 +0200

    Fixed nested <a> in sidebar

--- components/SidebarItem.tsx
+++ components/SidebarItem.tsx
@@ -63,9 +63,9 @@ export default function SidebarItem({
   return (
     <>
       <li>
-        <Link
-          href={subMenu ? '#' : url}
-          className={`
+        {subMenu ? (
+          <div
+            className={`
          group relative my-1 flex w-full cursor-pointer
          items-center rounded-md px-3
          py-2 font-medium transition-colors
@@ -76,58 +76,116 @@ export default function SidebarItem({
          }
           ${!expanded && 'flex'}
      `}
-          onClick={() => {
-            setExpandSubMenu((curr) => expanded && !curr);
-          }}
-        >
-          <span className='h-6 w-6'>{icon}</span>
-
-          <span
-            className={`overflow-hidden text-start transition-all ${
-              expanded ? 'ml-3 w-44' : 'w-0'
-            }`}
+            onClick={() => {
+              setExpandSubMenu((curr) => expanded && !curr);
+            }}
           >
-            {text}
-          </span>
-          {subMenu && (
-            <div
-              className={`absolute right-2 h-4 w-4${
-                expanded ? '' : 'top-2'
-              } transition-all ${expandSubMenu ? 'rotate-90' : 'rotate-0'}`}
-            >
-              <ChevronRightIcon height={20} />
-            </div>
-          )}
+            <span className='h-6 w-6'>{icon}</span>
 
-          {/* 
+            <span
+              className={`overflow-hidden text-start transition-all ${
+                expanded ? 'ml-3 w-44' : 'w-0'
+              }`}
+            >
+              {text}
+            </span>
+            {subMenu && (
+              <div
+                className={`absolute right-2 h-4 w-4${
+                  expanded ? '' : 'top-2'
+                } transition-all ${expandSubMenu ? 'rotate-90' : 'rotate-0'}`}
+              >
+                <ChevronRightIcon height={20} />
+              </div>
+            )}
+
+            {/* 
             display item text or sub-menu items when hovered
           */}
-          {!expanded && (
-            <div
-              className={`
+            {!expanded && (
+              <div
+                className={`
             text-primary-500 invisible absolute left-full ml-6 -translate-x-3
             rounded-md bg-indigo-100 px-2
             py-1 text-sm opacity-20 transition-all
             group-hover:visible group-hover:translate-x-0 group-hover:opacity-100
         `}
-            >
-              {/* 
+              >
+                {/* 
                 if hovered item has no sub-menu, display the text
                 else display the sub-menu items
               */}
-              {!subMenu
-                ? text
-                : subMenu.map((item, index) => (
-                    <HoveredSubMenuItem
-                      key={index}
-                      text={item.text}
-                      icon={item.icon}
-                      url={item.url}
-                    />
-                  ))}
-            </div>
-          )}
-        </Link>
+                {!subMenu
+                  ? text
+                  : subMenu.map((item, index) => (
+                      <HoveredSubMenuItem
+                        key={index}
+                        text={item.text}
+                        icon={item.icon}
+                        url={item.url}
+                      />
+                    ))}
+              </div>
+            )}
+          </div>
+        ) : (
+          <Link
+            href={subMenu ? '#' : url}
+            className={`
+         group relative my-1 flex w-full cursor-pointer
+         items-center rounded-md px-3
+         py-2 font-medium transition-colors
+         ${
+           active && !subMenu
+             ? 'text-primary-500 bg-gradient-to-tr from-indigo-200 to-indigo-100'
+             : 'text-gray-600 hover:bg-indigo-50'
+         }
+          ${!expanded && 'flex'}
+     `}
+            onClick={() => {
+              setExpandSubMenu((curr) => expanded && !curr);
+            }}
+          >
+            <span className='h-6 w-6'>{icon}</span>
+
+            <span
+              className={`overflow-hidden text-start transition-all ${
+                expanded ? 'ml-3 w-44' : 'w-0'
+              }`}
+            >
+              {text}
+            </span>
+            {/* 
+            display item text or sub-menu items when hovered
+          */}
+            {!expanded && (
+              <div
+                className={`
+            text-primary-500 invisible absolute left-full ml-6 -translate-x-3
+            rounded-md bg-indigo-100 px-2
+            py-1 text-sm opacity-20 transition-all
+            group-hover:visible group-hover:translate-x-0 group-hover:opacity-100
+        `}
+              >
+                {/* 
+                if hovered item has no sub-menu, display the text
+                else display the sub-menu items
+              */}{' '}
+                {text}
+                {/* {!subMenu
+                  ? text
+                  : subMenu.map((item, index) => (
+                      <HoveredSubMenuItem
+                        key={index}
+                        text={item.text}
+                        icon={item.icon}
+                        url={item.url}
+                      />
+                    ))} */}
+              </div>
+            )}
+          </Link>
+        )}
       </li>
       <ul className='sub-menu pl-6' style={{ height: subMenuHeight }}>
         {/* 
